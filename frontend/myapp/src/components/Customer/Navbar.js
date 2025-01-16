287857c9dcd3959dd7f4aa13a35321f88d69bd8e
sontruonghp2004 <tranvutruongson14062004@gmail.com>
2025-01-16 23:35:03 +0700

    fix navbar and replace new front

--- frontend/myapp/src/components/Customer/Navbar.js
+++ frontend/myapp/src/components/Customer/Navbar.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { toast } from 'react-toastify';
 import LoginModal from './LoginModel';
 
@@ -32,27 +31,51 @@ function Navbarr() {
 
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg-light shadow-sm">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          {/* Logo */}
+          <Navbar.Brand href="#home" className="d-flex align-items-center">
+            <img
+              src="images/logo.png"
+              alt="MomCare Logo"
+              width="40"
+              height="40"
+              className="me-2"
+            />
+            <span className="fw-bold">Mom & Baby</span>
+          </Navbar.Brand>
+          
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              {isLoggedIn ? (
-                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-              ) : (
-                <Nav.Link onClick={handleShow}>Login</Nav.Link>
-              )}
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
+
+{/* Navbar Links */}
+<Navbar.Collapse id="basic-navbar-nav">
+  <Nav className="mx-auto">
+    <Nav.Link href="#home">Trang chủ</Nav.Link>
+    <Nav.Link href="#features">Cộng Đồng</Nav.Link>
+    <Nav.Link href="#tracking">Theo dõi</Nav.Link>
+    <Nav.Link href="#blog">Blog</Nav.Link>
+  </Nav>
+
+  {/* Login/Logout and Register */}
+  <Nav className="ms-auto">
+    {/* Conditional Rendering for Login/Logout */}
+    {isLoggedIn ? (
+      <Nav.Link onClick={handleLogout} className="text-danger">
+        Đăng xuất
+      </Nav.Link>
+    ) : (
+      <Nav.Link onClick={handleShow} className="text-primary">
+        Đăng nhập
+      </Nav.Link>
+    )}
+    {/* Đăng ký */}
+    {!isLoggedIn && (
+      <Nav.Link href="#register" className="btn btn-success text-white ms-2">
+        Đăng ký
+      </Nav.Link>
+    )}
+  </Nav>
+</Navbar.Collapse>
         </Container>
       </Navbar>
 
